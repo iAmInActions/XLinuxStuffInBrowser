90d5a1e4f7794cba7a59cce69907a7774e0036eb
iAmInActions <83808704+iAmInActions@users.noreply.github.com>
2021-05-07 22:29:15 +0200

    Update lxde-vnc.sh

--- lxde-vnc.sh
+++ lxde-vnc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
+export HOME=/home/notroot
 tigervncserver -noxstartup -SecurityTypes None -geometry 1280x720 :0
 export DISPLAY=:0
-export HOME=/home/$1
 openbox &
 lxsession &
 lxterminal --command='bash --login' &
