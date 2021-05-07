9f886c53d7dc260abca1e06f1abd1bcc4ea828c6
iAmInActions <83808704+iAmInActions@users.noreply.github.com>
2021-05-07 20:46:33 +0200

    Update webdesktop.sh

--- webdesktop.sh
+++ webdesktop.sh
@@ -4,12 +4,15 @@ apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-d
 apt install -y openjdk-8-jre
 git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
-sudo apt install -y tigervnc-standalone-server lxde lxterminal
-tigervncserver -noxstartup -SecurityTypes None :0
+echo "Please select lightdm as the default display manager."
+apt install -y tigervnc-standalone-server lxde lxterminal
+useradd notroot
+clear
+echo "Enter a user password:"
+passwd notroot
+tigervncserver -noxstartup -SecurityTypes None -geometry 1280x720 :0
 export DISPLAY=:0
-openbox &
-lxsession &
-lxterminal &
+lightdm
 while true
 do
 sleep 1000
