4efc3789f3c03a2dd4a78032a0f1e6fc8b7e0b25
iAmInActions <83808704+iAmInActions@users.noreply.github.com>
2021-05-07 19:35:52 +0200

    Create minecraft_instant_vnc_setup.sh

---
+++ minecraft_instant_vnc_setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+apt update
+wget -O virtualgl.deb https://sourceforge.net/projects/virtualgl/files/2.6.5/virtualgl_2.6.5_amd64.deb/download
+apt install -y ./virtualgl.deb
+apt install -y xvfb x11vnc build-essential libx11-dev libxcursor-dev libxrandr-dev libxinerama-dev libxi-dev libgl1-mesa-dev libgl1-mesa-dri python-minimal
+apt install -y openjdk-8-jre
+git clone https://github.com/ayunami2000/noVNC
+git clone https://github.com/mindstorm38/portablemc
+./noVNC/utils/launch.sh --listen 80 &
+Xvfb -screen 0 900x720x24+32 +extension GLX &
+export DISPLAY=:0
+x11vnc -noshm -geometry 900x720 -shared -forever &
+yes y | python3.7 ./portablemc/portablemc.py start -u appointturtle 1.12.2
