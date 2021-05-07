add0517f5f6ea1d55231058e8237866c47301ce0
iAmInActions <83808704+iAmInActions@users.noreply.github.com>
2021-05-07 22:29:34 +0200

    Update webdesktop.sh

--- webdesktop.sh
+++ webdesktop.sh
@@ -14,7 +14,7 @@ sleep 4
 clear
 wget https://raw.githubusercontent.com/iAmInActions/UsefullScripts/main/lxde-vnc.sh
 chmod +x ./lxde-vnc.sh
-sudo -u notroot ./lxde-vnc.sh &
+sudo -u notroot ./lxde-vnc.sh
 bash --login
 while true
 do
