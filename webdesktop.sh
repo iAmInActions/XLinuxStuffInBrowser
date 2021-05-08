f4ac78c3851119b755d90d2f8ed290be856f1955
iAmInActions <83808704+iAmInActions@users.noreply.github.com>
2021-05-08 22:23:04 +0200

    Added audio

--- webdesktop.sh
+++ webdesktop.sh
@@ -6,7 +6,7 @@ git clone https://github.com/ayunami2000/noVNC
 ./noVNC/utils/launch.sh --listen 80 &
 echo "Please select gdm3 as the default display manager."
 sleep 3
-apt install -y tigervnc-standalone-server lxde lxterminal firefox
+apt install -y tigervnc-standalone-server lxde lxterminal firefox alsa-base alsa-tools pulseaudio
 useradd notroot
 echo 'notroot  ALL=(ALL:ALL) ALL' >> /etc/sudoers
 echo "webtop" | passwd --stdin notroot
@@ -16,6 +16,16 @@ clear
 mkdir /home/notroot/
 chown notroot /home/notroot/
 cd /home/notroot/
+
+# Create audio stream
+modprobe snd-aloop pcm_substreams=1
+echo "# .asoundrc" >> /etc/asound.conf
+echo "pcm.!default { type plug slave.pcm "hw:Loopback,0,0" }" >> /etc/asound.conf
+mkdir /root/noVNC/app/desktopaudio/
+cd /root/noVNC/app/desktopaudio/
+ffmpeg -f alsa -channels 2 -sample_rate 44100 -i hw:Loopback,1,0 -map 0 -codec:a aac -f ssegment -segment_list stream.m3u -segment_list_flags +live -segment_time 10 out%03d.ts &
+cd /home/notroot/
+
 wget https://raw.githubusercontent.com/iAmInActions/UsefullScripts/main/lxde-vnc.sh
 chmod +x ./lxde-vnc.sh
 sudo -u notroot ./lxde-vnc.sh
